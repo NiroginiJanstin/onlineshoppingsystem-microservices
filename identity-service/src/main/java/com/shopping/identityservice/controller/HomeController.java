6f9c0aa3ebd71ec86057f6ddab2cadc478a7cb32
Nirogini_105758 <nirogini.albertjanstin@axiatadigitallabs.com>
2022-05-16 01:44:31 +0530

    Identity service issue fixed

--- identity-service/src/main/java/com/shopping/identityservice/controller/HomeController.java
+++ identity-service/src/main/java/com/shopping/identityservice/controller/HomeController.java
@@ -5,16 +5,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.shopping.identityservice.model.JwtRequest;
 import com.shopping.identityservice.model.JwtResponse;
 import com.shopping.identityservice.service.UserService;
 import com.shopping.identityservice.utility.JWTUtility;
 
 @RestController
+@RequestMapping("identity")
 public class HomeController {
 
     @Autowired
