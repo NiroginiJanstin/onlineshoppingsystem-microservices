6764d330542f9788a8b9c2f76d431e2c1e766ef3
unknown <piranavanath@codelantic.com>
2022-05-16 21:44:42 +0530

    customer change

--- customer-service/src/main/java/com/shopping/customerservice/controller/CustomerController.java
+++ customer-service/src/main/java/com/shopping/customerservice/controller/CustomerController.java
@@ -4,37 +4,61 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.shopping.customerservice.model.Customer;
+import com.shopping.customerservice.repository.CustomerRepository;
 import com.shopping.customerservice.service.CustomerService;
 
 import java.util.List;
 
-@RequestMapping("/api/employee")
+@RequestMapping("/api/customer")
 @RestController
 public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+    
+    @Autowired
+    CustomerRepository repo;
 
-    @PostMapping
-    public int addCustomer(@RequestBody Customer customer)
+    @PostMapping("/save")
+    public Customer addCustomer(@RequestBody Customer customer)
     {
-        return customerService.addCustomer(customer);
+    	Customer cus = repo.save(customer);
+        return cus;
     }
-    @GetMapping("/emp")
-    public List<Customer> getAllCustomer()
+    
+    @GetMapping("/getAll")
+    public List<Customer> listAll() {
+        List<Customer> listUsers = repo.findAll();
+        return listUsers;
+    }
+ 
+    @GetMapping(path = "/getById")
+    public Customer getCustomer(@RequestParam("id") int id)
     {
-        return customerService.getAllCustomer();
+    	Customer cus = repo.getById(id);
+    	return cus;
     }
 
-    @GetMapping(path = "{id}")
-    public Customer getCustomer(@PathVariable("id") int id)
+    @DeleteMapping(path = "/delete")
+    public String deleteCustomer(@RequestParam("id") int id)
     {
-        return customerService.getCustomer(id);
+    	repo.deleteById(id);
+        return  "Customer Id " + id + " is Deleted!!";
     }
-
-    @DeleteMapping(path = "{id}")
-    public int deleteCustomer(@PathVariable("id") int id)
+    
+    @PutMapping(path = "/update")
+    public Customer updateCustomer(@RequestBody Customer customer)
     {
-        return  customerService.deleteCustomer(id);
+    	Customer cus = new Customer();
+    	cus.setCustomer_id(customer.getCustomer_id());
+    	cus.setName(customer.getName());
+    	cus.setPhone(customer.getPhone());
+    	cus.setAddress(customer.getAddress());
+    	cus.setCustomer_code(customer.getCustomer_code());
+    	Customer cust = repo.save(cus);
+    	return cust;
+        
     }
+    
+ 
 }
--- customer-service/src/main/java/com/shopping/customerservice/model/Customer.java
+++ customer-service/src/main/java/com/shopping/customerservice/model/Customer.java
@@ -1,7 +1,17 @@
 package com.shopping.customerservice.model;
 
-public class Customer {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "customer")
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
     private int customer_id;
     private String name;
     private int phone;
